89536597abb23ddc51bc6ba61a1de34d9ef93ce6
koke-desu <gbpvp25645@yahoo.co.jp>
2021-02-18 23:46:03 +0900

    type Post modification

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,25 +3,17 @@ import firebase from "firebase";
 
 // 初期化は環境変数を使うため、サーバーサイドで行う必要があったので、
 // index.tsxのgetStaticPropsで行っている。
-export const DB = firebase.firestore();
 
 // ここから型の定義。
 // Firebaseの方でフィールド追加したら、こちらも変更すべし。
-// タグやカテゴリーに名前以外のデータを後ほど追加するかもしれないので、別のコレクションとして管理しているが、
-// 名前しか必要無いようなら、post内でstringとして受け取るように変更する。->そのほうが獲得楽だし、データ読み取り回数も少ない。
-export type Tag = {
-  name: string;
-};
-
-export type Category = {
-  name: string;
-};
+// tag, categoryは現在stringの配列として管理している。名前以外の情報を管理するようになったら、
+// firebaseの方で参照型か、オブジェクト型にする。
 
 export type Post = {
   title: string;
   body: string;
-  category: Category;
-  tag: Tag;
+  category: string[];
+  tag: string[];
   createTime: Date;
   updateTime: Date;
 };
