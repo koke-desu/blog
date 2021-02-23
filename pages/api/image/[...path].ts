3170124bf0ce87cdfeba3cb70e85af407ba5b0b1
koke-desu <gbpvp25645@yahoo.co.jp>
2021-02-23 23:45:07 +0900

    create image api.

---
+++ pages/api/image/[...path].ts
@@ -0,0 +1,20 @@
+//import { NextApiRequest, NextApiResponse } from 'next'
+import { getImage } from "../../../firebase/dbFunctions";
+import admin from "firebase-admin";
+
+export default async (req, res) => {
+  const {
+    query: { path },
+  } = req;
+  const Storage: admin.storage.Storage = admin.storage();
+  res.status(200).json({
+    url: await getImage(Storage, path.join("/"))
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        console.log("image get err.\n" + err);
+        return "image not found";
+      }),
+  });
+};
