c4fcd84ef93d3b1ea562769afb2e3f6e3f012df8
koke-desu <gbpvp25645@yahoo.co.jp>
2021-02-25 22:54:50 +0900

    add color config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main1: "#30A9F4",
+        main2: "#039BE5",
+        accent: "#FFC107",
+      },
+    },
   },
   variants: {
     extend: {},
