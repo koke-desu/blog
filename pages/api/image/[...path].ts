810ae81f1c9f844e2c9a32668ddaf54b552ac9ab
koke-desu <gbpvp25645@yahoo.co.jp>
2021-02-26 16:34:31 +0900

    improve firebase initialization.

--- pages/api/image/[...path].ts
+++ pages/api/image/[...path].ts
@@ -6,9 +6,9 @@ export default async (req, res) => {
   const {
     query: { path },
   } = req;
-  const Storage: admin.storage.Storage = admin.storage();
+  //const Storage: admin.storage.Storage = admin.storage();
   res.status(200).json({
-    url: await getImage(Storage, path.join("/"))
+    url: await getImage(path.join("/"))
       .then((res) => {
         return res;
       })
