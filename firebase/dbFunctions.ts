90cfbf267acd167f8b34dfc9799e688ca141b48d
koke-desu <gbpvp25645@yahoo.co.jp>
2021-03-01 23:19:31 +0900

    add getCategory getTag.

--- firebase/dbFunctions.ts
+++ firebase/dbFunctions.ts
@@ -1,16 +1,12 @@
 // firebaseの読み書きを行う関数を定義。
 // すべてサーバーサイドで呼び出すこと！
+// getStaticPropsで呼び出すか、apiルート経由で呼び出す。
 import firebase from "firebase";
-import { Post, Category } from "./firebase";
+import { Post, Category, Tag, Tags } from "./firebase";
 import admin from "./firebase_init";
 
-// 記事の一覧を渡す。Promiseオブジェクトなので、
-//   await getPosts().then((res) => {
-//     return res;
-//   })
-//  みたいにして受け取る！！。
-// awaitつけないとgetStaticPropsで受け取れない。
-export const getPosts = async () => {
+// 記事の一覧を渡す。
+export const getAllPosts = async () => {
   const DB = admin.firestore();
   const data: Post[] = [];
 
@@ -32,6 +28,30 @@ export const getPosts = async () => {
       });
     });
   return data;
+};
+
+// idを指定して一つ記事を獲得。
+export const getPost = async (id: string) => {
+  const DB = admin.firestore();
+  const snapshot = await DB.collection("posts").doc(id).get();
+
+  //記事が存在しない場合エラーを投げる。
+  if (snapshot.empty) {
+    throw "post not found";
+  }
+
+  const post_data = snapshot.data();
+  const post: Post = {
+    id: snapshot.id,
+    title: post_data.title,
+    body: post_data.body,
+    createTime: post_data.createTime.toDate(),
+    updateTime: post_data.updateTime.toDate(),
+    tag: post_data.tag,
+    category: post_data.category,
+  };
+
+  return post;
 };
 
 // admin側で使う関数。
@@ -74,11 +94,58 @@ export const getImage = async (path: string) => {
     action: "read",
     expires: "12-31-2121",
   });
-  //console.log(img_url);
+
   return img_url.toString();
 };
 
-//
-export const getCategorys = async (DB: FirebaseFirestore.Firestore) => {
-  //const data: Category[] =
+// すべてのカテゴリーを獲得する。
+export const getCategories = async () => {
+  const DB = admin.firestore();
+  const data: Category[] = [];
+
+  await DB.collection("categories")
+    .get()
+    .then((categories) => {
+      categories.forEach((category) => {
+        const category_tmp: Category = {
+          id: category.id,
+          name: category.data().name,
+        };
+        data.push(category_tmp);
+      });
+    });
+  return data;
+};
+
+// 全てのタグを獲得する。タグは種類ごとに2重のオブジェクトで管理される。Type Tagsを参照。
+export const getTags = async () => {
+  const DB: FirebaseFirestore.Firestore = admin.firestore();
+  const data: Tags[] = [];
+
+  await DB.collection("tags")
+    .get()
+    .then((tags) => {
+      tags.forEach((group) => {
+        const tags_tmp: Tags = {
+          id: group.id,
+          name: group.data().name,
+          children: [],
+        };
+        data.push(tags_tmp);
+      });
+    });
+
+  for (let i = 0; i < data.length; i++) {
+    await DB.collection("tags")
+      .doc(data[i].id)
+      .collection("children")
+      .get()
+      .then((tags) => {
+        tags.forEach((tag) => {
+          data[i].children.push({ id: tag.id, name: tag.data().name });
+        });
+      });
+  }
+
+  return data;
 };
