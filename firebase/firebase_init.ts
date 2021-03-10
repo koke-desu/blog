ae1b284c6caa4349fd83f852a86177a6e1970405
koke-desu <gbpvp25645@yahoo.co.jp>
2021-03-10 18:14:03 +0900

    adapt for vercel enviroment value.

--- firebase/firebase_init.ts
+++ firebase/firebase_init.ts
@@ -1,9 +1,20 @@
 const admin = require("firebase-admin");
-const ServiceAccount = require("../firebase/serviceAccountKey.json");
+//const ServiceAccount = require("../firebase/serviceAccountKey.json");
 
 try {
   admin.initializeApp({
-    credential: admin.credential.cert(ServiceAccount),
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
