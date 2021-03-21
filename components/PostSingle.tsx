501157fe061cb089d89e9dc233e2ebcbef480a4e
koke-desu <gbpvp25645@yahoo.co.jp>
2021-03-21 21:27:26 +0900

    improve PostSingle style.

--- components/PostSingle.tsx
+++ components/PostSingle.tsx
@@ -11,27 +11,75 @@ import style from "./css/markdown_style.module.css";
 
 export default function PostSingle({ post }: { post: Post }) {
   return (
-    <div className={style.markdown_body + " markdown-body p-6 bg-white mt-5"}>
-      <div>
-        <Image src={post.thumbnail} height={100} width={100} />
-        <h1>{post.title}</h1>
+    <div className={`mt-10 p-10 bg-white relative`}>
+      <div className="flex items-center justify-between">
+        <h1 className="my-5 font-bold text-4xl">{post.title}</h1>
+        <div className="flex  pr-1 items-center">
+          {(() => {
+            const update = new Date(post.updateTime);
+            const create = new Date(post.createTime);
+            if (update.toString() != create.toString()) {
+              return (
+                <>
+                  <Image
+                    src="/images/update_icon.png"
+                    width={20}
+                    height={20}
+                    layout="fixed"
+                  />
+                  <p className="text-base ml-1">
+                    {update.getFullYear() +
+                      "/" +
+                      (update.getMonth() + 1) +
+                      "/" +
+                      update.getDate()}
+                  </p>
+                </>
+              );
+            } else {
+              return (
+                <>
+                  <Image src="/images/create_icon.png" width={20} height={16} />
+                  <p>
+                    {update.getFullYear() +
+                      "/" +
+                      (update.getMonth() + 1) +
+                      "/" +
+                      update.getDate()}
+                  </p>
+                </>
+              );
+            }
+          })()}
+        </div>
       </div>
-      <div className="flex h-8">
-        <Image src="/images/tag_icon.png" width={25} height={26} className="" />
+
+      <div className="flex h-8 items-center">
+        <Image
+          src="/images/tag_icon.png"
+          width={20}
+          height={20}
+          layout="fixed"
+        />
         {post.tag.map((tag, index) => {
           return (
             <Link href="/" key={`tag_${tag}`}>
-              <p className="p-0.5 ml-1 cursor-pointer self-auto text-lg">
+              <p className="px-1 ml-1 cursor-pointer self-auto text-lg border-b-2 border-white hover:border-gray-400 hover:text-gray-400">
                 {index > 0 ? ", " + tag : tag}
               </p>
             </Link>
           );
         })}
       </div>
-      <ReactMarkdown plugins={[[gfm]]} allowDangerousHtml>
-        {/* サーバー側で、"\\n"にエスケープされた改行を"/n"にする。 */}
-        {post.body}
-      </ReactMarkdown>
+      <div className="w-full h-96 relative">
+        <Image src={post.thumbnail} layout="fill" className="object-contain" />
+      </div>
+      <article className={`mt-10 markdown-body ${style.markdown_body}`}>
+        <ReactMarkdown plugins={[[gfm]]} allowDangerousHtml>
+          {/* サーバー側で、"\\n"にエスケープされた改行を"/n"にする。 */}
+          {post.body}
+        </ReactMarkdown>
+      </article>
     </div>
   );
 }
